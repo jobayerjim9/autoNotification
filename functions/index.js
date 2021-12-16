e95d01f1217d3649619181f4d1346eb49c57caa9
Jobaer Hossain <jobayerjim9@gmail.com>
2021-12-16 22:57:33 +0600

    - Tweaked

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,138 @@ const axios = require("axios");
 // schedule("0 8 * * *")
 // schedule("every 5 minutes")
 admin.initializeApp();
+let header;
+let userPosition=0;
+const users = [];
+const processData =
+    (body, totalCount, willSend, child, followedVal,
+        position, resolve, token) => {
+      if (position<followedVal.length) {
+        const followedId = followedVal[position];
+        const url = "https://api.spotify.com/v1/artists/" + followedId + "/albums?limit=50&include_groups=album,single,appears_on";
+        return new Promise((resolve, reject) => {
+          axios.get(url, header)
+              .then((response) => {
+                const items = response.data["items"];
+                for (let i = 0; i < items.length; i++) {
+                  const item = items[i];
+                  const releaseDate =
+                            // eslint-disable-next-line max-len
+                            new Date(item["release_date"].toString());
+                  const today = new Date();
+                  const diffTime =
+                            Math.abs(today - releaseDate);
+                  const diffDays =
+                            // eslint-disable-next-line max-len
+                            Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                  if (diffDays <= 1) {
+                    const artists = item["artists"];
+                    for (let j = 0; j < artists.length; j++) {
+                      if (followedId === artists[j].id) {
+                        // eslint-disable-next-line max-len
+                        willSend = true;
+                        totalCount++;
+                        if (totalCount <= 2) {
+                          if (totalCount === 1) {
+                            body = body + " " + artists[j].name + ",";
+                          } else {
+                            body = body + " " + artists[j].name;
+                          }
+                        }
+                        console.log("artistName "+body);
+                      }
+                    }
+                  }
+                }
+                processData(body,
+                    totalCount,
+                    willSend,
+                    child,
+                    followedVal,
+                    position + 1,
+                    resolve,
+                    token
+                );
+              }).catch((err) => {
+                if (position===followedVal.length) {
+                  iterateByUser(users, userPosition + 1, resolve);
+                } else {
+                  processData(body,
+                      totalCount,
+                      willSend,
+                      child,
+                      followedVal,
+                      position + 1,
+                      resolve,
+                      token
+                  );
+                }
+                console.log("processDataError "+err.data);
+              });
+        });
+      } else {
+        if (willSend) {
+          const titleT = "New Music Released Today";
+          if (totalCount===1) {
+            body=body.replace(",", "");
+          }
+          totalCount = totalCount - 2;
+          if (totalCount > 0) {
+            body = body + " and " + totalCount + " More";
+          }
+          const payload = {
+            data: {
+              title: titleT,
+              body: body,
+            },
+          };
+          admin.messaging()
+              .sendToDevice(token, payload);
+          console
+              .log("noti sent" +
+                      token);
+          console.log("payload " + body);
+        }
+        console.log("outside "+position+" "+userPosition);
+        if (userPosition===users.length) {
+          resolve("200");
+        } else {
+          iterateByUser(users, userPosition + 1, resolve);
+        }
+      }
+    };
+
+const iterateByUser = (users, position, resolve) => {
+  userPosition=position;
+  if (position<users.length) {
+    userPosition=position;
+    const child=users[position];
+    const tokenL = child
+        .child("notificationToken").val();
+    console.log("executing "+tokenL);
+    const body="Music by";
+    const totalCount=0;
+    const willSend = false;
+    const followed=child.child("followedArtists");
+    if (followed.hasChildren()) {
+      // eslint-disable-next-line max-len
+      const followedVal=child.child("followedArtists").val();
+      processData(body,
+          totalCount,
+          willSend,
+          child,
+          followedVal,
+          0,
+          resolve,
+          tokenL
+      );
+    } else {
+      iterateByUser(users, userPosition + 1, resolve);
+    }
+  } else {
+    resolve("200");
+  }
+};
 
 exports.autoNotification =
     functions.pubsub.schedule("0 8 * * *")
@@ -27,77 +159,20 @@ exports.autoNotification =
             axios.post("https://accounts.spotify.com/api/token", params, config)
                 .then((result) => {
                   const accessToken= "Bearer "+result.data.access_token;
-                  const header = {
+                  header = {
                     headers: {
                       "Authorization": accessToken,
                     },
                   };
-                  axios.get("https://api.spotify.com/v1/browse/new-releases?limit=50", header)
-                      .then((response) => {
-                        const albums=response.data["albums"];
-                        root.child("userInfo").once("value").then((snap) => {
-                          if (snap.hasChildren()) {
-                            snap.forEach((child)=> {
-                              let body="Music by";
-                              let totalCount=0;
-                              let willSend = false;
-                              for (let i =0; i<albums.items.length; i++) {
-                                const item=albums.items[i];
-                                const releaseDate =
-                                new Date(item["release_date"].toString());
-                                const today = new Date();
-                                const diffTime = Math.abs(today - releaseDate);
-                                const diffDays =
-                                    Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                                if (diffDays<=1) {
-                                  const followed=child.child("followedArtists");
-                                  if (followed.hasChildren()) {
-                                    const artists=item["artists"];
-                                    followed.forEach((followedArtists)=> {
-                                      const followedId= followedArtists.val();
-                                      for (let j=0; j<artists.length; j++) {
-                                        if (followedId === artists[j].id) {
-                                          // eslint-disable-next-line max-len
-                                          willSend=true;
-                                          totalCount++;
-                                          if (totalCount<=2) {
-                                            if (totalCount===1) {
-                                              body=body+" "+artists[j].name+",";
-                                            } else {
-                                              body=body+" "+artists[j].name;
-                                            }
-                                          }
-                                        }
-                                      }
-                                    });
-                                  }
-                                }
-                              }
-                              if (willSend) {
-                                const titleT = "New Music Released Today";
-                                totalCount = totalCount - 2;
-                                if (totalCount>0) {
-                                  body = body + " and " + totalCount + " More";
-                                }
-                                const payload = {
-                                  data: {
-                                    title: titleT,
-                                    body: body,
-                                  },
-                                };
-                                const token = child
-                                    .child("notificationToken").val();
-                                admin.messaging()
-                                    .sendToDevice(token, payload);
-                                console
-                                    .log("noti sent" +
-                                          token);
-                              }
-                            });
-                          }
-                          resolve("200");
-                        });
+
+                  root.child("userInfo").once("value").then((snap) => {
+                    if (snap.hasChildren()) {
+                      snap.forEach((child)=> {
+                        users.push(child);
                       });
+                      iterateByUser(users, 0, resolve);
+                    }
+                  });
                 })
                 .catch((err) => {
                   console.log(err.data);
