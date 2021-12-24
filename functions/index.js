3a3699b044fd171afde77cd81aaf251cc573bfa1
jobayerjim9 <mhossain172104@bscse.uiu.ac.bd>
2021-12-24 17:17:30 +0600

    - Optimized

--- functions/index.js
+++ functions/index.js
@@ -21,116 +21,116 @@ const processData =
           let lastArtistName = "";
           axios.get(url, header)
               .then((response) => {
-                  let artistAlreadyFound = false;
-                  const items = response.data["items"];
-                  for (let i = 0; i < items.length; i++) {
-                      const item = items[i];
-                      const releaseDate =
+                let artistAlreadyFound = false;
+                const items = response.data["items"];
+                for (let i = 0; i < items.length; i++) {
+                  const item = items[i];
+                  const releaseDate =
                           // eslint-disable-next-line max-len
                           new Date(item["release_date"].toString());
-                      const today = new Date();
-                      const diffTime =
+                  const today = new Date();
+                  const diffTime =
                           Math.abs(today - releaseDate);
-                      const diffDays =
+                  const diffDays =
                           // eslint-disable-next-line max-len
                           Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                      const artists = item["artists"];
+                  const artists = item["artists"];
                   if (diffDays <= 1 && diffDays>=0) {
                     for (let j = 0; j < artists.length; j++) {
                       if (followedId === artists[j].id) {
-                          // eslint-disable-next-line max-len
-                          willSend = true;
-                          artistAlreadyFound = true;
-                          if (!body.includes(artists[j].name)) {
-                              totalCount++;
-                              if (totalCount <= 2) {
-                                  if (totalCount === 1) {
-                                      body = body + " " + artists[j].name + ",";
-                                  } else {
-                                      body = body + " " + artists[j].name;
-                                  }
-                              }
-                              console.log("artistName " + body);
+                        // eslint-disable-next-line max-len
+                        willSend = true;
+                        artistAlreadyFound = true;
+                        if (!body.includes(artists[j].name)) {
+                          totalCount++;
+                          if (totalCount <= 2) {
+                            if (totalCount === 1) {
+                              body = body + " " + artists[j].name + ",";
+                            } else {
+                              body = body + " " + artists[j].name;
+                            }
+                          }
+                          console.log("artistName " + body);
                         }
                       }
                     }
                   } else {
-                      for (let j = 0; j < artists.length; j++) {
-                          if (followedId === artists[j].id) {
-                              lastArtistName = artists[j].name;
-                          }
+                    for (let j = 0; j < artists.length; j++) {
+                      if (followedId === artists[j].id) {
+                        lastArtistName = artists[j].name;
                       }
+                    }
                   }
-                  }
-                  if (!artistAlreadyFound) {
-                      const fUrl = "https://api.spotify.com/v1/artists/" + followedId + "/albums?limit=50&include_groups=appears_on";
-                      axios.get(fUrl, header)
-                          .then((response) => {
-                              const items = response.data["items"];
-                              for (let i = 0; i < items.length; i++) {
-                                  const item = items[i];
-                                  const releaseDate =
+                }
+                if (!artistAlreadyFound) {
+                  const fUrl = "https://api.spotify.com/v1/artists/" + followedId + "/albums?limit=50&include_groups=appears_on";
+                  axios.get(fUrl, header)
+                      .then((response) => {
+                        const items = response.data["items"];
+                        for (let i = 0; i < items.length; i++) {
+                          const item = items[i];
+                          const releaseDate =
                                       // eslint-disable-next-line max-len
                                       new Date(item["release_date"].toString());
-                                  const today = new Date();
-                                  const diffTime =
+                          const today = new Date();
+                          const diffTime =
                                       Math.abs(today - releaseDate);
-                                  const diffDays =
+                          const diffDays =
                                       // eslint-disable-next-line max-len
                                       Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                                  if (diffDays <= 1 && diffDays >= 0) {
-                                      const artists = item["artists"];
-                                      if (artists[0].name !== "Various Artists") {
-                                          willSend = true;
-                                          if (!body.includes(lastArtistName)) {
-                                              totalCount++;
-                                              if (totalCount <= 2) {
-                                                  if (totalCount === 1) {
-                                                      body = body + " " + lastArtistName + ",";
-                                                  } else {
-                                                      body = body + " " + lastArtistName;
-                                                  }
-                                              }
-                                          }
-                                      }
+                          if (diffDays <= 1 && diffDays >= 0) {
+                            const artists = item["artists"];
+                            if (artists[0].name !== "Various Artists") {
+                              willSend = true;
+                              if (!body.includes(lastArtistName)) {
+                                totalCount++;
+                                if (totalCount <= 2) {
+                                  if (totalCount === 1) {
+                                    body = body + " " + lastArtistName + ",";
+                                  } else {
+                                    body = body + " " + lastArtistName;
                                   }
+                                }
                               }
-                              processData(body,
-                                  totalCount,
-                                  willSend,
-                                  child,
-                                  followedVal,
-                                  position + 1,
-                                  resolve,
-                                  token
-                              );
-                          }).catch((err) => {
-                          if (position === followedVal.length) {
-                              iterateByUser(users, userPosition + 1, resolve);
-                          } else {
-                              processData(body,
-                                  totalCount,
-                                  willSend,
-                                  child,
-                                  followedVal,
-                                  position + 1,
-                                  resolve,
-                                  token
-                              );
+                            }
                           }
-                          console.log("processDataError " + err.data);
+                        }
+                        processData(body,
+                            totalCount,
+                            willSend,
+                            child,
+                            followedVal,
+                            position + 1,
+                            resolve,
+                            token
+                        );
+                      }).catch((err) => {
+                        if (position === followedVal.length) {
+                          iterateByUser(users, userPosition + 1, resolve);
+                        } else {
+                          processData(body,
+                              totalCount,
+                              willSend,
+                              child,
+                              followedVal,
+                              position + 1,
+                              resolve,
+                              token
+                          );
+                        }
+                        console.log("processDataError " + err.data);
                       });
-                  } else {
-                      processData(body,
-                          totalCount,
-                          willSend,
-                          child,
-                          followedVal,
-                          position + 1,
-                          resolve,
-                          token
-                      );
-                  }
+                } else {
+                  processData(body,
+                      totalCount,
+                      willSend,
+                      child,
+                      followedVal,
+                      position + 1,
+                      resolve,
+                      token
+                  );
+                }
               }).catch((err) => {
                 if (position===followedVal.length) {
                   iterateByUser(users, userPosition + 1, resolve);
@@ -164,19 +164,19 @@ const processData =
               body: body,
             },
           };
-            admin.messaging()
-                .sendToDevice(token, payload);
-            console
-                .log("noti sent" +
+          admin.messaging()
+              .sendToDevice(token, payload);
+          console
+              .log("noti sent" +
                     token);
-            console.log("payload " + body);
+          console.log("payload " + body);
         }
-          console.log("outside " + position + " " + userPosition);
-          if (userPosition >= users.length) {
-              resolve("200");
-          } else {
-              iterateByUser(users, userPosition + 1, resolve);
-          }
+        console.log("outside " + position + " " + userPosition);
+        if (userPosition >= users.length) {
+          resolve("200");
+        } else {
+          iterateByUser(users, userPosition + 1, resolve);
+        }
       }
     };
 
@@ -205,10 +205,10 @@ const iterateByUser = (users, position, resolve) => {
           tokenL
       );
     } else {
-        iterateByUser(users, userPosition + 1, resolve);
+      iterateByUser(users, userPosition + 1, resolve);
     }
   } else {
-      resolve("200");
+    resolve("200");
   }
 };
 // schedule("0 8 * * *")
@@ -216,22 +216,22 @@ const iterateByUser = (users, position, resolve) => {
 exports.autoNotification =
     functions.pubsub.schedule("0 8 * * *")
         .timeZone("Europe/London").onRun((context) => {
-        const root = admin.database().ref();
-        const params = new URLSearchParams();
-        params.append("grant_type", "client_credentials");
-        // eslint-disable-next-line max-len
-        const basicToken = "Basic OWJlYjRiZDI4MDQ4NGRhZDkxODYzZDE5NDFkZGQ3OTg6OGZjODc0ODQ1MDk1NGE3ZmIxYWUyZTI5OWQzMTI0NTE=";
-        const config = {
+          const root = admin.database().ref();
+          const params = new URLSearchParams();
+          params.append("grant_type", "client_credentials");
+          // eslint-disable-next-line max-len
+          const basicToken = "Basic OWJlYjRiZDI4MDQ4NGRhZDkxODYzZDE5NDFkZGQ3OTg6OGZjODc0ODQ1MDk1NGE3ZmIxYWUyZTI5OWQzMTI0NTE=";
+          const config = {
             headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": basicToken,
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Authorization": basicToken,
             },
-        };
-        return new Promise((resolve, reject) => {
+          };
+          return new Promise((resolve, reject) => {
             axios.post("https://accounts.spotify.com/api/token", params, config)
                 .then((result) => {
-                    const accessToken = "Bearer " + result.data.access_token;
-                    header = {
+                  const accessToken = "Bearer " + result.data.access_token;
+                  header = {
                     headers: {
                       "Authorization": accessToken,
                     },
